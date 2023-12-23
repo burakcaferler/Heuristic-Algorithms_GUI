56d895b6f9a2c04cf2b1b0f75624f84199b27bbd
burakcaferler <104163715+burakcaferler@users.noreply.github.com>
2023-12-23 20:03:40 +0300

    v4 with WOA but WOA is not working

---
+++ v4/enumFunctions.py
@@ -0,0 +1,13 @@
+import enum
+
+class Functions(enum.IntEnum):
+    ackley = 0
+    griewank = 1
+    schwefel = 2
+    rastrigin = 3
+    sphere = 4
+    perm = 5
+    zakharov = 6
+    rosenbrock = 7
+    dixonprice = 8
+ 
